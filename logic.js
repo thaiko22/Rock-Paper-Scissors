8df160657b72136e6e2bf0514ba92cfb873f85ca
thaiko22 <106668301+thaiko22@users.noreply.github.com>
2026-05-28 23:53:46 +0200

    Created an UI for the Game

--- logic.js
+++ logic.js
@@ -39,6 +39,49 @@ function getHumanChoice () {
     return options[choice-1]
 }
 
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+rock.addEventListener("click", () => {playRound("rock",getComputerChoice())})
+paper.addEventListener("click", () => {playRound("paper",getComputerChoice())})
+scissors.addEventListener("click", ()=> {playRound("scissors",getComputerChoice())})
+const counter = document.querySelector("#counter")
+const sH = document.querySelector("#humie")
+const sP = document.querySelector("#pc")
+let humic = 0;
+let pcc = 0;
+const comment = document.querySelector("#comment")
+counter.addEventListener("Roundplayed", (event) => {
+    
+    if (event.detail.whoWon == 0) {
+        ++pcc
+        comment.textContent = "Computer Won!"
+    }
+    else if (event.detail.whoWon == 1) {
+        ++humic
+        comment.textContent = "Player Won!"
+    }
+
+    else {
+        comment.textContent = "Thats a draw!"
+    }
+    
+    if (humic == 5) {
+        comment.textContent= "Humie Won the Game " + humic + " : " + pcc + "!!" 
+        humic= 0
+        pcc = 0
+        
+    }
+    if (pcc == 5) {
+        comment.textContent= "Computer Won the Game " + humic + " : " + pcc + "!!" 
+        pcc = 0
+        humic = 0
+
+    }
+    sH.textContent = humic
+    sP.textContent = pcc
+})
+
 function playRound (humi, pc){
     /*
         play Round:
@@ -46,60 +89,49 @@ function playRound (humi, pc){
         rock wins against scissors, scissors wins against paper, paper wins against stone
         if humi wins, print return True else false
      */
-    if (humi == "rock" && pc == "scissors") { return true}
-    if (humi == "paper" && pc == "rock") { return true}
-    if (humi == "scissors" && pc == "paper") {return true}
-    else { return false}
+    let win = 0
+    if (humi == "rock" && pc == "scissors") { 
+        let humiWon = new CustomEvent("Roundplayed", { detail: {
+            whoWon: 1
+        }})
+        counter.dispatchEvent(humiWon)
+    }
+    else if (humi == "paper" && pc == "rock") { 
+        let humiWon = new CustomEvent("Roundplayed", { detail: {
+            whoWon: 1
+        }})
+        counter.dispatchEvent(humiWon)
+    }
+    else if (humi == "scissors" && pc == "paper") {
+        let humiWon = new CustomEvent("Roundplayed", { detail: {
+            whoWon: 1
+        }})
+        counter.dispatchEvent(humiWon)
+
+    }
+    else if (humi == pc) { 
+        let draw = new CustomEvent("Roundplayed", { detail: {
+            whoWon: 2
+        }})
+        counter.dispatchEvent(draw)
+
+    }
+    else {
+        let pcWon = new CustomEvent("Roundplayed", { detail: {
+            whoWon: 0
+        }})
+        counter.dispatchEvent(pcWon)
+
+    }
+    
 }   
 
 
 
-function playGame() {
-    /*
-        play game:
-        ask for amount of games the player wants to play.
-        convert input to int
-        initialize a counter for player and Computer
-        if player wins, ad 1 to player counter
-        if computer wins, ad 1 to computer counter
-        after each round, print the counter
-
-        
-     */
-    playerc = 0
-    computerc = 0
-    amountofgames = parseInt(prompt("how many games do you want to play?"))
-    for (let i = 0 ; i < amountofgames; i++) {
-        player = getHumanChoice()
-        computer = getComputerChoice()
-
-        console.log("player = "+ player + " computer = " + computer)
-        if (playRound(player,computer)) {
-            playerc++
-            console.log("You won this round!!")
-        }
-        else if (player == computer) {
-            console.log("Thats a tie!!")
-        }   
-        else {
-            computerc++
-            console.log("You lost this round...")
-        }
-        console.log(playerc + " : " + computerc)
-    }
-
-    if (playerc > computerc) {
-        console.log("You won " + playerc + " to " + computerc + "!!")
-
-    }
-    else if (playerc==computerc){
-        console.log("thats a tie!! " + playerc + " : " + computerc )
-    }
-    else {
-        console.log("You lost " + playerc + " to " + computerc + "...")
-    }
-}
 
 
 
-playGame()
+
+
+
+
